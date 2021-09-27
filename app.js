1feb973ed8cba9e1e1c52fce9f090bca29cf5578
Evan Loiterman <evanloiterman@gmail.com>
2021-09-27 11:42:35 -0500

    add db

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var config = require('./config.dev');
+
+//Test the file
+
+
+
 var app = express();
 
 // view engine setup
